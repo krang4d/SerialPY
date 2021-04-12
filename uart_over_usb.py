4616d9345db3d0b6f40854e3d2e7a0598eb70f7a
pgg <jzi@inbox.ru>
2021-04-13 00:30:36 +0300

    chore(uart_over_usb): close event, @staticmethod is_open

--- uart_over_usb.py
+++ uart_over_usb.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 from abc import ABCMeta, abstractmethod
 from datetime import datetime
 # Create an Iterator for scale X on plot
-class Numbers:
+class NumbersIterator:
     def __iter__(self):
         self.a = 1
         return self
@@ -23,53 +23,60 @@ class Numbers:
         self.a += 1
         return x
 
-class Plotter(metaclass=ABCMeta):
+class Plotter:
+    _iter = iter(NumbersIterator())
+    _stop = True
+    _num = 100
 
-    _figure = None
-    _ax1, _ax2, _ax3 = None, None, None
+    @staticmethod
+    def is_open() -> bool:
+        return Plotter._stop
 
-    @abstractmethod
     def update(line: str):
-        ...
+        print(line)
+
+    def _on_close(self, event):
+        print('Closed Figure!')
+        Plotter._stop = False
 
 class MatPlotter(Plotter):
+    _figure = None
+    _ax1, _ax2 = None, None
+    _line1, _line2, line3 = None, None, None
+    _x1 = np.zeros(Plotter._num, dtype=int)
+    _y1 = np.zeros(Plotter._num)
+    _x2 = np.zeros(Plotter._num, dtype=int)
+    _y2 = np.zeros(Plotter._num)
+    _x3 = np.zeros(Plotter._num, dtype=int)
+    _y3 = np.zeros(Plotter._num)
+    _x4 = np.zeros(Plotter._num, dtype=int)
+    _y4 = np.zeros(Plotter._num)
+
     def __init__(self):
-        self.num = 100
-        self._x1 = np.zeros(self.num, dtype=int)
-        self._y1 = np.zeros(self.num)
-        self._x2 = np.zeros(self.num, dtype=int)
-        self._y2 = np.zeros(self.num)
-        self._x3 = np.zeros(self.num, dtype=int)
-        self._y3 = np.zeros(self.num)
-        self._x4 = np.zeros(self.num, dtype=int)
-        self._y4 = np.zeros(self.num)
-        self.iter = iter(Numbers())
         #turns on the interactive mode
         plt.ion()
-        
-        self.figure, (self.ax1, self.ax2, self.ax3 ) = plt.subplots( 1, 3, figsize=(15,5) )
+
+        self.figure, (self.ax1, self.ax2 ) = plt.subplots( 1, 2, figsize=(15,5) )
         self.figure.suptitle("Dynamic UART",fontsize=14)
+
+        self.figure.canvas.mpl_connect('close_event', self._on_close)
 
         self.ax1.grid()
         self.ax2.grid()
-        self.ax3.grid()
 
         self.line1, = self.ax1.plot(self._x1, self._y1)
         self.line2, = self.ax2.plot(self._x2, self._y2)
-        self.line3, = self.ax3.plot(self._x3, self._y3)
+        self.line3, = self.ax2.plot(self._x3, self._y3)
 
-        self.ax1.set_xlabel('Time, sec',fontsize=18)
+        self.ax1.set_xlabel('Step',fontsize=18)
         self.ax1.set_ylabel('RPM',fontsize=18)
 
-        self.ax2.set_xlabel('Temperature, C',fontsize=18)
-        self.ax2.set_ylabel('ADC',fontsize=18)
-
-        self.ax3.set_xlabel('Time, sec',fontsize=18)
-        self.ax3.set_ylabel('ADC/50, T (C)',fontsize=18)
+        self.ax2.set_xlabel('Step',fontsize=18)
+        self.ax2.set_ylabel('ADC/50, T (C)',fontsize=18)
 
     def update(self, Y):
         try:
-            index = next(self.iter)
+            index = next(Plotter._iter)
             self._y1 = np.delete(self._y1, 0)
             self._x1 = np.delete(self._x1, 0)
             self._y1 = np.append(self._y1, Y[0])
@@ -94,24 +101,21 @@ class MatPlotter(Plotter):
 
             dy = .5
             self.ax1.set_ylim([np.min(self._y1)-dy, np.max(self._y1)+dy])
-            self.ax1.set_xlim([self._x1[0], self._x1[self.num-1]])
-            self.ax2.set_ylim([np.min(self._y2)-dy, np.max(self._y2)+dy])
-            self.ax2.set_xlim([self._x2[0], self._x2[self.num-1]])
-            self.ax3.set_ylim([np.min(self._y3)-dy, np.max(self._y3)+dy])
-            self.ax3.set_xlim([self._x3[0], self._x3[self.num-1]])
+            self.ax1.set_xlim([self._x1[0], self._x1[Plotter._num-1]])
+
+            minimum = np.min(np.concatenate((self._y2, self._y3), axis=None ))-dy
+            maximum = np.max(np.concatenate((self._y2, self._y3), axis=None ))+dy
+            self.ax2.set_ylim([minimum, maximum])
+            self.ax2.set_xlim([self._x2[0], self._x2[Plotter._num-1]])
 
             self.figure.canvas.draw()
-            plt.pause(0.001)
             self.figure.canvas.flush_events()
-            #time.sleep(0.1)
+            Plotter.update(Y)
+            plt.pause(0.005)
+
         except Exception as e:
-            print(e)
-            exit()
-
-class DrawPlotter(Plotter):
-
-    def update(self, line):
-        print(line)
+           print(e)
+           exit()
 
 class _LoggerCallback(metaclass=ABCMeta):
     @abstractmethod
@@ -125,14 +129,14 @@ class _FileLoggerCallback(_LoggerCallback):
 
 class UDevice:
     _port = None
-    #_logger = None
+    _logger = None
     def __init__(self, baudrate: int, timeout: int, logger : _LoggerCallback):
         UDevice._logger = logger
         UDevice._port = serial.Serial(port=self._get_device(), baudrate=baudrate, timeout=timeout)
 
     def __del__(self):
         if UDevice._port is not None:
-            print('Close port')
+            print('Closed Port!')
             UDevice._port.close()
 
     def _get_device(self):
@@ -143,7 +147,7 @@ class UDevice:
             #print(pt)
             if 'USB' or 'ACM'  in pt[1]:
                 devices.append(pt)
-                print('%d. %s %s'%(index, pt[1], pt[0]))
+                print('%d. %s %s'%(index, pt[0], pt[1]))
 
         if len(devices)==0:
             print('No USB device found.')
@@ -173,12 +177,10 @@ if __name__ == "__main__":
     chp = MatPlotter()
 
     device = UDevice(baudrate, timeout, _FileLoggerCallback())
-    while(True):
+    while(chp.is_open()):
         line = device.readline()
-        print(line)
         chp.update(line)
     del device
-
 
     # except serial.SerialException as e:
     #     print('error open serial port: ' + str(e))
