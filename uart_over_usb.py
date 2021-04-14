5fe52f0a1630a53dc34134be3e1b766a445b9c74
pgg <jzi@inbox.ru>
2021-04-14 17:48:02 +0300

    update for stm32f407

--- uart_over_usb.py
+++ uart_over_usb.py
@@ -26,7 +26,8 @@ class NumbersIterator:
 class Plotter:
     _iter = iter(NumbersIterator())
     _stop = True
-    _num = 100
+
+    _num = 100 # число отсчетов по оси Х на графиках
 
     @staticmethod
     def is_open() -> bool:
@@ -42,7 +43,7 @@ class Plotter:
 class MatPlotter(Plotter):
     _figure = None
     _ax1, _ax2 = None, None
-    _line1, _line2, line3 = None, None, None
+    _line1, _line2, _line3 = None, None, None
     _x1 = np.zeros(Plotter._num, dtype=int)
     _y1 = np.zeros(Plotter._num)
     _x2 = np.zeros(Plotter._num, dtype=int)
@@ -52,27 +53,37 @@ class MatPlotter(Plotter):
     _x4 = np.zeros(Plotter._num, dtype=int)
     _y4 = np.zeros(Plotter._num)
 
-    def __init__(self):
+    def __init__(self, title=""):
         #turns on the interactive mode
         plt.ion()
 
-        self.figure, (self.ax1, self.ax2 ) = plt.subplots( 1, 2, figsize=(15,5) )
-        self.figure.suptitle("Dynamic UART",fontsize=14)
+        self._figure, (self._ax1, self._ax2 ) = plt.subplots( 1, 2, figsize=(8,4) )
 
-        self.figure.canvas.mpl_connect('close_event', self._on_close)
+        self._figure.canvas.set_window_title("Dynamic serial port")
+        self._figure.suptitle(title,fontsize=14)
 
-        self.ax1.grid()
-        self.ax2.grid()
+        self._figure.canvas.mpl_connect('close_event', self._on_close)
 
-        self.line1, = self.ax1.plot(self._x1, self._y1)
-        self.line2, = self.ax2.plot(self._x2, self._y2)
-        self.line3, = self.ax2.plot(self._x3, self._y3)
+        self._ax1.grid()
+        self._ax2.grid()
 
-        self.ax1.set_xlabel('Step',fontsize=18)
-        self.ax1.set_ylabel('RPM',fontsize=18)
+        self._line1, = self._ax1.plot(self._x1, self._y1)
+        self._line2, = self._ax2.plot(self._x2, self._y2)
+        self._line3, = self._ax2.plot(self._x3, self._y3)
 
-        self.ax2.set_xlabel('Step',fontsize=18)
-        self.ax2.set_ylabel('ADC/50, T (C)',fontsize=18)
+        self._line1.set_label('RPM')
+        self._line2.set_label('T(C)')
+        self._line3.set_label('ADC')
+
+        self._ax1.legend()
+        self._ax2.legend()
+
+        self._ax1.set_xlabel('Step',fontsize=12)
+        self._ax1.set_ylabel('RPM',fontsize=12)
+
+        self._ax2.set_xlabel('Step',fontsize=12)
+        self._ax2.set_ylabel('ADC, T (C)',fontsize=12)
+        plt.tight_layout()
 
     def update(self, Y):
         try:
@@ -84,32 +95,34 @@ class MatPlotter(Plotter):
 
             self._y2 = np.delete(self._y2, 0)
             self._x2 = np.delete(self._x2, 0)
-            self._y2 = np.append(self._y2, Y[1])
+            self._y2 = np.append(self._y2, Y[2])
             self._x2 = np.append(self._x2, index)
 
             self._y3 = np.delete(self._y3, 0)
             self._x3 = np.delete(self._x3, 0)
-            self._y3 = np.append(self._y3, Y[2])
+            self._y3 = np.append(self._y3, Y[3])
             self._x3 = np.append(self._x3, index)
 
-            self.line1.set_xdata(self._x1)
-            self.line1.set_ydata(self._y1)
-            self.line2.set_xdata(self._x2)
-            self.line2.set_ydata(self._y2)
-            self.line3.set_xdata(self._x3)
-            self.line3.set_ydata(self._y3)
+            self._line1.set_xdata(self._x1)
+            self._line1.set_ydata(self._y1)
+            self._line2.set_xdata(self._x2)
+            self._line2.set_ydata(self._y2)
+            self._line3.set_xdata(self._x3)
+            self._line3.set_ydata(self._y3)
 
-            dy = .5
-            self.ax1.set_ylim([np.min(self._y1)-dy, np.max(self._y1)+dy])
-            self.ax1.set_xlim([self._x1[0], self._x1[Plotter._num-1]])
 
-            minimum = np.min(np.concatenate((self._y2, self._y3), axis=None ))-dy
-            maximum = np.max(np.concatenate((self._y2, self._y3), axis=None ))+dy
-            self.ax2.set_ylim([minimum, maximum])
-            self.ax2.set_xlim([self._x2[0], self._x2[Plotter._num-1]])
+            dy1 = (np.max(self._y1) - np.min(self._y1))/100
+            minimum = np.min(np.concatenate((self._y2, self._y3), axis=None ))
+            maximum = np.max(np.concatenate((self._y2, self._y3), axis=None ))
+            dy2 = (maximum - minimum)/100
 
-            self.figure.canvas.draw()
-            self.figure.canvas.flush_events()
+            self._ax1.set_ylim([np.min(self._y1)-dy1, np.max(self._y1)+dy1])
+            self._ax1.set_xlim([self._x1[0], self._x1[Plotter._num-1]])
+            self._ax2.set_ylim([minimum-dy2, maximum]+dy2)
+            self._ax2.set_xlim([self._x2[0], self._x2[Plotter._num-1]])
+
+            self._figure.canvas.draw()
+            self._figure.canvas.flush_events()
             Plotter.update(Y)
             plt.pause(0.005)
 
@@ -123,16 +136,24 @@ class _LoggerCallback(metaclass=ABCMeta):
         ...
 
 class _FileLoggerCallback(_LoggerCallback):
+    _file_name = "uart.log" # имя лог файла
+
     def __call__(self, data: str):
-        with open("uart.log", 'a') as f:
+        with open(self._file_name, 'a') as f:
             f.write('%s %.3f;%.3f;%.3f\n'%(datetime.now(), data[0], data[1], data[2]))
 
 class UDevice:
     _port = None
     _logger = None
+    _name = None
+
     def __init__(self, baudrate: int, timeout: int, logger : _LoggerCallback):
         UDevice._logger = logger
-        UDevice._port = serial.Serial(port=self._get_device(), baudrate=baudrate, timeout=timeout)
+        try:
+            UDevice._port = serial.Serial(port=self._get_device(), baudrate=baudrate, timeout=timeout)
+        except Exception as e:
+           print(e.strerror)
+           exit()
 
     def __del__(self):
         if UDevice._port is not None:
@@ -158,7 +179,8 @@ class UDevice:
         elif len(devices)>1:
             x=int(input('Input the device number: '))
 
-        return devices[x][0]
+        self._name = devices[x][0]
+        return self._name
 
     def readline(self) ->str:
         data = UDevice._port.readline()
@@ -174,11 +196,13 @@ if __name__ == "__main__":
     # Define UART settings
     baudrate = 9200
     timeout = 3
-    chp = MatPlotter()
 
     device = UDevice(baudrate, timeout, _FileLoggerCallback())
+    chp = MatPlotter(device._name)
+
     while(chp.is_open()):
         line = device.readline()
+        line[2] = line[2]/16
         chp.update(line)
     del device
 
